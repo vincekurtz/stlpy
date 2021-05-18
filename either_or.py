7c4a0e563f23d01709cb219e7a1be232b01b2c2d
Vince Kurtz <vjkurtz@gmail.com>
2021-05-18 16:20:35 -0400

    found bug in perspective MICP on either-or

--- either_or.py
+++ either_or.py
@@ -11,14 +11,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scenarios.either_or import either_or_specification, plot_either_or_scenario
-from solvers import MICPSolver, GradientSolver
+from solvers import MICPSolver, GradientSolver, PerspectiveMICPSolver
 
 # Specification Parameters
 goal = (7,8,8,9)     # (xmin, xmax, ymin, ymax)
 target_one = (1,2,6,7)
 target_two = (7,8,4.5,5.5)
 obstacle = (3,5,4,6)
-T = 20
+T = 12
 
 # The "big-M" constant used for mixed-integer encoding
 M = 1000
@@ -46,6 +46,7 @@ x0 = np.array([1.0,1.0,0,0])
 
 # Solve for the system trajectory
 solver = MICPSolver(spec, A, B, Q, R, x0, T, M)
+#solver = PerspectiveMICPSolver(spec, A, B, Q, R, x0, T)
 #solver = GradientSolver(spec, A, B, Q, R, x0, T)
 x, u = solver.Solve()
 
