71c42377014f63dd05fb629c1b1e9d7192ec9bb1
Vince Kurtz <vjkurtz@gmail.com>
2022-03-30 14:54:35 -0400

    remove knitro solver

--- solvers/__init__.py
+++ solvers/__init__.py
@@ -1,7 +1,6 @@
 from solvers.base import STLSolver
 from solvers.scipy.gradient_solver import ScipyGradientSolver
 from solvers.gurobi.gurobi_micp import GurobiMICPSolver
-from solvers.knitro.knitro_lcp import KnitroLCPSolver
 from solvers.drake.drake_micp import DrakeMICPSolver
 from solvers.drake.drake_smooth import DrakeSmoothSolver
 from solvers.drake.drake_sos1 import DrakeSos1Solver
