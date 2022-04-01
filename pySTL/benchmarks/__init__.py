db54f32ba44ca0ce57d927278cb5bf9f0da09168
Vince Kurtz <vjkurtz@gmail.com>
2022-04-01 16:44:57 -0400

    refactor narrow passage benchmark

--- pySTL/benchmarks/__init__.py
+++ pySTL/benchmarks/__init__.py
@@ -1,3 +1,4 @@
 from .reach_avoid import ReachAvoid
 from .either_or import EitherOr
+from .narrow_passage import NarrowPassage
 from .door_puzzle import DoorPuzzle
--- pySTL/benchmarks/narrow_passage.py
+++ pySTL/benchmarks/narrow_passage.py
@@ -1,75 +1,72 @@
-from .common import (inside_rectangle_formula,
-                     outside_rectangle_formula,
-                     make_rectangle_patch)
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
-##
-#
-# Tools for setting up a slightly more complex specification where a robot
-# with double integrator dynamics must navigate through several obstacles
-# with narrow passages before reaching one of several possible goals.
-#
-##
+from .base import BenchmarkScenario
+from .common import (inside_rectangle_formula,
+                     outside_rectangle_formula,
+                     make_rectangle_patch)
+from ..systems import DoubleIntegrator
 
-def narrow_passage_specification(T):
+class NarrowPassage(BenchmarkScenario):
+    r"""
+    A 2D mobile robot with double integrator dynamics must navigate around
+    several obstacles (:math:`\mathcal{O}_i`) before reaching one of two
+    goals (:math:`\mathcal{G}_i`).
+
+    .. math::
+
+        \varphi = F_{[0,T]}(\mathcal{G}_1 \lor \mathcal{G}_2) \land
+            G_{[0,T]} \left( \bigwedge_{i=1}^4 \lnot \mathcal{O}_i \right)
+
+    :param T:   The time horizon of the specification.
     """
-    Return an STLFormula that describes this scenario, with time bound T.
+    def __init__(self, T):
+        # Define obstacle and goal regions by their bounds,
+        # (xmin, xmax, ymin, ymax)
+        self.obstacles = [(2,5,4,6),
+                          (5.5,9,3.8,5.7),
+                          (4.6,8,0.5,3.5),
+                          (2.2,4.4,6.4,11)]
+        self.goals = [(7,8,8,9),
+                      (9.5,10.5,1.5,2.5)]
+        self.T = T
 
-    We'll assume that the robot has double integrator dynamics, i.e.,
+    def GetSpecification(self):
+        # Goal Reaching
+        goal_formulas = []
+        for goal in self.goals:
+            goal_formulas.append(inside_rectangle_formula(goal, 0, 1, 6))
 
-        x = [px,py,pdx,pdy], u = [pddx, pddy]
+        at_any_goal = goal_formulas[0]
+        for i in range(1,len(goal_formulas)):
+            at_any_goal = at_any_goal | goal_formulas[i]
 
-    and that the output signal is given by y = [x;u].
-    """
-    obstacles = [(2,5,4,6),
-                 (5.5,9,3.8,5.7),
-                 (4.6,8,0.5,3.5),
-                 (2.2,4.4,6.4,11)]
-    goals = [(7,8,8,9),
-             (9.5,10.5,1.5,2.5)]
+        # Obstacle Avoidance
+        obstacle_formulas = []
+        for obs in self.obstacles:
+            obstacle_formulas.append(outside_rectangle_formula(obs, 0, 1, 6))
 
-    # Goal Reaching
-    goal_formulas = []
-    for goal in goals:
-        goal_formulas.append(inside_rectangle_formula(goal, 0, 1, 6))
+        obstacle_avoidance = obstacle_formulas[0]
+        for i in range(1, len(obstacle_formulas)):
+            obstacle_avoidance = obstacle_avoidance & obstacle_formulas[i]
 
-    at_any_goal = goal_formulas[0]
-    for i in range(1,len(goal_formulas)):
-        at_any_goal = at_any_goal | goal_formulas[i]
+        # Put all of the constraints together in one specification
+        specification = at_any_goal.eventually(0, self.T) & \
+                        obstacle_avoidance.always(0, self.T)
 
-    # Obstacle Avoidance
-    obstacle_formulas = []
-    for obs in obstacles:
-        obstacle_formulas.append(outside_rectangle_formula(obs, 0, 1, 6))
+        return specification
 
-    obstacle_avoidance = obstacle_formulas[0]
-    for i in range(1, len(obstacle_formulas)):
-        obstacle_avoidance = obstacle_avoidance & obstacle_formulas[i]
+    def GetSystem(self):
+        return DoubleIntegrator(2)
 
-    # Put all of the constraints together in one specification
-    specification = at_any_goal.eventually(0,T) & \
-                    obstacle_avoidance.always(0,T)
+    def add_to_plot(self, ax):
+        # Make and add rectangular patches
+        for obstacle in self.obstacles:
+            ax.add_patch(make_rectangle_patch(*obstacle, color='k', alpha=0.5))
+        for goal in self.goals:
+            ax.add_patch(make_rectangle_patch(*goal, color='green', alpha=0.5))
 
-    return specification
-
-def plot_narrow_passage_scenario():
-    ax = plt.gca()
-
-    obstacles = [(2,5,4,6),
-                 (5.5,9,3.8,5.7),
-                 (4.6,8,0.5,3.5),
-                 (2.2,4.4,6.4,11)]
-    goals = [(7,8,8,9),
-             (9.5,10.5,1.5,2.5)]
-
-    # Make and add rectangular patches
-    for obstacle in obstacles:
-        ax.add_patch(make_rectangle_patch(*obstacle, color='k', alpha=0.5))
-    for goal in goals:
-        ax.add_patch(make_rectangle_patch(*goal, color='green', alpha=0.5))
-
-    # set the field of view
-    ax.set_xlim((0,12))
-    ax.set_ylim((0,12))
-    ax.set_aspect('equal')
+        # set the field of view
+        ax.set_xlim((0,12))
+        ax.set_ylim((0,12))
+        ax.set_aspect('equal')
