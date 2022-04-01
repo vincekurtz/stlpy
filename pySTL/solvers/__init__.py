060863ec2bdf81efbaab56413ac7a5593699d27d
Vince Kurtz <vjkurtz@gmail.com>
2022-04-01 11:15:33 -0400

    make solver-specific dependencies optional

--- pySTL/solvers/__init__.py
+++ pySTL/solvers/__init__.py
@@ -1,7 +1,33 @@
-from .scipy.gradient_solver import ScipyGradientSolver
+# Test whether various optional dependencies are installed
+DRAKE_ENABLED = True
+try:
+    import pydrake
+except ImportError:
+    print("WARNING: pydrake import failed, drake-based solvers disabled.\n")
+    DRAKE_ENABLED = False
 
-from .gurobi.gurobi_micp import GurobiMICPSolver
+SCIPY_ENABLED = True
+try:
+    import scipy
+except ImportError:
+    print("WARNING: scipy import failed, scipy-based solvers disabled.\n")
+    SCIPY_ENABLED = False
 
-from .drake.drake_micp import DrakeMICPSolver
-from .drake.drake_smooth import DrakeSmoothSolver
-from .drake.drake_sos1 import DrakeSos1Solver
+GUROBI_ENABLED = True
+try:
+    import gurobipy
+except ImportError:
+    print("WARNING: gurobi import failed, gurobi-based solvers disabled.\n")
+    GUROBI_ENABLED = False
+
+# And load the corresponding solvers accordingly
+if SCIPY_ENABLED:
+    from .scipy.gradient_solver import ScipyGradientSolver
+
+if GUROBI_ENABLED:
+    from .gurobi.gurobi_micp import GurobiMICPSolver
+
+if DRAKE_ENABLED:
+    from .drake.drake_micp import DrakeMICPSolver
+    from .drake.drake_smooth import DrakeSmoothSolver
+    from .drake.drake_sos1 import DrakeSos1Solver
