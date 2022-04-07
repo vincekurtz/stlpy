1a23d44fb318d73ee6d7755497e608d69b90e8df
Vince Kurtz <vjkurtz@gmail.com>
2022-04-07 10:39:49 -0400

    more verbose import warnings

--- stlpy/solvers/__init__.py
+++ stlpy/solvers/__init__.py
@@ -3,21 +3,28 @@ DRAKE_ENABLED = True
 try:
     import pydrake
 except ImportError:
-    print("WARNING: pydrake import failed, drake-based solvers disabled.\n")
+    print("WARNING: pydrake import failed, Drake-based solvers disabled.")
+    print("         Install drake (https://drake.mit.edu/installation.html)")
+    print("         to use the Drake-based solvers.")
     DRAKE_ENABLED = False
 
 SCIPY_ENABLED = True
 try:
     import scipy
 except ImportError:
-    print("WARNING: scipy import failed, scipy-based solvers disabled.\n")
+    print("WARNING: scipy import failed, scipy-based solvers disabled.")
+    print("         Install scipy (https://scipy.org/install/)")
+    print("         to use the scipy-based solvers.")
     SCIPY_ENABLED = False
 
 GUROBI_ENABLED = True
 try:
     import gurobipy
 except ImportError:
-    print("WARNING: gurobi import failed, gurobi-based solvers disabled.\n")
+    print("WARNING: gurobipy import failed, Gurobi-based solvers disabled.")
+    print("         Install Gurobi (https://www.gurobi.com/documentation/)")
+    print("         to use the Gurobi-based solvers. Free academic licenses")
+    print("         are available.")
     GUROBI_ENABLED = False
 
 # And load the corresponding solvers accordingly
