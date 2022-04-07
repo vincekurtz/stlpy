9f11f85b638300551bf431fbd68b43d7ddf48926
Vince Kurtz <vjkurtz@gmail.com>
2022-04-07 10:57:13 -0400

    update project description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='stlpy',
       version='0.1.0',
-      description='A Python library for Signal Temporal Logic',
+      description='A Python library for control from Signal Temporal Logic (STL) specifications',
       url='http://github.com/vincekurtz/stlpy',
       author='Vince Kurtz',
       author_email='vjkurtz@gmail.com',
